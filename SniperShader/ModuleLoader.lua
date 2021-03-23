8a98cf358da25a5fe685288694f27ad14df22403
CenteredSniper <59752762+CenteredSniper@users.noreply.github.com>
2021-03-23 02:49:53 -0700

    Update ModuleLoader.lua

--- SniperShader/ModuleLoader.lua
+++ SniperShader/ModuleLoader.lua
@@ -1,5 +1,8 @@
+print(1)
 if _G.loadmode then
+  print(2)
   if _G.loadmode["Shaders"] then
+    print(3)
     loadstring(game:HttpGet('https://github.com/CenteredSniper/Kenzen/blob/master/SniperShader/Shaders.lua'))()
   if _G.loadmode["MusicUI"] then
     loadstring(game:HttpGet('https://github.com/CenteredSniper/Kenzen/blob/master/SniperShader/MusicUI.lua'))()
