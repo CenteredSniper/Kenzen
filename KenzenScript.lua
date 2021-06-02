87e492bed0e802f23fc3b090d0fe8b20e750b891
CenteredSniper <59752762+CenteredSniper@users.noreply.github.com>
2021-06-01 22:24:55 -0700

    Update KenzenScript.lua

--- KenzenScript.lua
+++ KenzenScript.lua
@@ -1,4 +1,4 @@
-print("Version: " .. 1 .. "." .. 4 .. "." .. 2)
+print("Kenzen // V: " .. 1 .. "." .. 4 .. "." .. 3)
 --= Start Up =--
 if _G.KenzenLoaded then error("kenzen already running") return end
 _G.KenzenLoaded = true
