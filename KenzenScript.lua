720819f27378d90b89827932106a08e09c999cb1
CenteredSniper <59752762+CenteredSniper@users.noreply.github.com>
2021-06-06 11:46:03 -0700

    Update KenzenScript.lua

--- KenzenScript.lua
+++ KenzenScript.lua
@@ -1,4 +1,4 @@
-local RTYGFHSWRGYJJRTG = "Kenzen // V: " .. 1 .. "." .. 5 .. "." .. 3
+local RTYGFHSWRGYJJRTG = "Kenzen // V: " .. 1 .. "." .. 5 .. "." .. 4
 if printconsole then printconsole(RTYGFHSWRGYJJRTG) else print(RTYGFHSWRGYJJRTG) end
 --= Start Up =--
 if _G.KenzenLoaded then error("kenzen already running") return end
