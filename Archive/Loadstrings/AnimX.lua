e9d82f024df3bfcee3f3d6c7c1fb83122bf34116
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2024-05-20 05:13:39 -0700

    Create AnimX.lua

---
+++ Archive/Loadstrings/AnimX.lua
@@ -0,0 +1,49 @@
+--[[ // METADATA
+
+Last Modified: 10/24/2020 6:30PM
+Author: Me
+
+--]]
+
+_G.animationstoadd = {
+    {4686555525,'Renegade',5382629149},
+    {5813142376,'Jerk it 2',0},
+    {4558409610,'Breakdown',0},
+    {3259216638,'Default',2338397562},
+    {3450125501,'S M U G',5418515711},
+    {4109162757,'Torture Dance',4938423365},
+    {4835795413,'unnamed',0},
+    {5813273319,'Bend',0},
+    {3619935462,'ElectroShuffle',0},
+    {3643129020,'Crabby',3399508747},
+    {5238669868,'Goopie',5177150121},
+    {3262592866,'Orange Justice',1840761620},
+    {5813270580,'Suck',0},
+    {5813333594,'Thrust',0},
+    {5362230261,'cruise',0},
+    {5713511509,'whip',959353963},
+    {5813332505,'Jerk it',0},
+    {4300223810,'R6 Garry Dance',0},
+    {4787449997,'KAZOTSKY',4884264021}, --2468295011
+    {5704448301,'Sitting',0},
+    {5619022453,"Sitting2",0},
+    {4687065078,'Dab',0},
+    {4686994115,'caramell',1027622568},
+    {4686894545,'carlton',2637402986},
+    {5596247646,'lol',0},
+    {5596237395,'distract',5629867848},
+    {5596229257,'clubpenguin',224774717},
+    {3445962423,'abdgangdance',4938423365},
+    {2545056106,'Work it Out',0},
+    {1567532145,'Shoot Dance',0},
+    {3468920161,'Ric Flair',0},
+    {3313251300,'Revenge',3317691529},
+    {1512792506,'Posedown',0},
+    {4517776376,'Peter Parker',4517951384},
+    {2052894017,'L',2658246719},
+    {3399569948,'Incredible',0},
+    {4221787882,'Hoedown',0},
+    {3583287353,'Floss',5201820685},
+    {2052902103,"Flippin' Epic",0},
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CenteredSniper/Kenzen/master/AnimXlol.lua", true))()
