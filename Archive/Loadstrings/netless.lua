58750904c142a472b4109bf71f8f56d78e3973b1
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2024-05-20 07:09:20 -0700

    Create netless.lua

---
+++ Archive/Loadstrings/netless.lua
@@ -0,0 +1,24 @@
+--[[ // METADATA
+
+Last Modified: 2/24/2022 8:04PM
+Author: ???
+
+--]]
+
+getgenv().Fling = false
+getgenv().TorsoFling = false
+getgenv().ShowReal = false
+getgenv().FakeGod = false
+getgenv().GodMode = true
+getgenv().Tools = true
+getgenv().Velocity = -25.05
+getgenv().Collisions = true
+getgenv().Claim2 = false
+getgenv().ExtremeNetless = false
+getgenv().Notification = true
+getgenv().AutoAnimate = true
+getgenv().DynamicVelocity = false
+getgenv().Network = true
+getgenv().AntiSleep = false
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CenteredSniper/Kenzen/master/newnetlessreanimate.lua"))()
