e61bebaa4c60f1ee442d2a70729f613d5c8ffb66
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2024-05-20 05:05:42 -0700

    Create Sniper's Logger.lua

---
+++ Archive/Loadstrings/Sniper's Logger.lua
@@ -0,0 +1,8 @@
+--[[ // METADATA
+
+Last Modified: 12/23/2020 4:26PM
+Author: Me
+
+--]]
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/CenteredSniper/Kenzen/master/SnipersLogger.lua'))()
