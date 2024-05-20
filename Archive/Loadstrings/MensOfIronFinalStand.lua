9a2d85722bc7e06fe3650ef69ad17188ea0d051e
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2024-05-20 04:55:35 -0700

    Create MensOfIronFinalStand.lua

---
+++ Archive/Loadstrings/MensOfIronFinalStand.lua
@@ -0,0 +1,8 @@
+--[[ // METADATA
+
+Last Modified: 10/25/2020 2:36AM
+Author: Me
+
+--]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CenteredSniper/Kenzen/master/MensOfIronFinalStand.lua", true))()
