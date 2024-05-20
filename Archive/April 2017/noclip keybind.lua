640ef5a6cdfe417c5355e7a4b030d346e3ad44d6
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2024-05-20 06:20:20 -0700

    Create noclip keybind.lua

---
+++ Archive/April 2017/noclip keybind.lua
@@ -0,0 +1,24 @@
+--[[ // METADATA
+
+Last Modified: 4/4/2017 9:03
+Author: ???
+
+--]]
+
+noclip = false
+game:GetService('RunService').Stepped:connect(function()
+if noclip then
+game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+end
+end)
+plr = game.Players.LocalPlayer
+mouse = plr:GetMouse()
+mouse.KeyDown:connect(function(key)
+
+if key == "e" then
+noclip = not noclip
+game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+end
+end)
+print('Loaded')
+print('Press "E" to noclip')
