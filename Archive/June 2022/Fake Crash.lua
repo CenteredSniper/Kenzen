862893c086aaf1d6e81d9f5adcdc50620d032691
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2024-05-20 07:48:02 -0700

    Create Fake Crash.lua

---
+++ Archive/June 2022/Fake Crash.lua
@@ -0,0 +1,40 @@
+--[[ // METADATA
+
+Last Modified: 6/20/2022 12:00PM
+Author: ???
+
+--]]
+
+local coregui = game:service'CoreGui'
+
+local sound = Instance.new('Sound', coregui)
+sound.SoundId = 'rbxassetid://865599816'
+sound.Volume = 3
+sound.Looped = true
+
+local gui = Instance.new('ScreenGui', coregui)
+local image = Instance.new('ImageLabel', gui)
+image.Position = UDim2.new(0, 0, -0.05, 0)
+image.Size = UDim2.new(1, 0, 1.05, 0)
+image.Image = "rbxassetid://9637192164"
+image.Visible = false
+
+task.wait(1)
+
+for i,v in pairs(coregui:GetChildren()) do
+    if v ~= gui and v ~= sound then
+        v:Destroy()   
+    end
+end
+
+if (not UserSettings().GameSettings:InFullScreen()) then
+    game:service'GuiService':ToggleFullscreen()    
+end
+
+game:service'UserInputService'.MouseIconEnabled = false
+sound:Play()
+image.Visible = true
+
+task.wait(1)
+
+while true do end
