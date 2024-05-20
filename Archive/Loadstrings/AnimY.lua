eca9b8d4273e569f97ec0c2a1a35a6a701d41a44
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2024-05-20 05:14:10 -0700

    Create AnimY.lua

---
+++ Archive/Loadstrings/AnimY.lua
@@ -0,0 +1,8 @@
+--[[ // METADATA
+
+Last Modified: 10/26/2021 4:57AM
+Author: Me
+
+--]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CenteredSniper/Kenzen/master/AnimY.lua", true))()
