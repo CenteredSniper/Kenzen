099f6223afb39480e5431b6558253e5c7b7e9889
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2024-05-20 05:06:38 -0700

    Create Steve's One Piece GUI.lua

---
+++ Archive/Loadstrings/Steve's One Piece GUI.lua
@@ -0,0 +1,8 @@
+--[[ // METADATA
+
+Last Modified: 8/14/2020 6:58PM
+Author: ???
+
+--]]
+
+spawn(loadstring(game:HttpGet('https://gist.githubusercontent.com/SadBinz/355853d993622d9596ee3351bf11a06e/raw')))
