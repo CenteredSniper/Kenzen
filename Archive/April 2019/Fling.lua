d7c31676fddda3eb724e9b0f2cd6ebb6ddcd45eb
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2024-05-20 06:12:33 -0700

    Create Fling.lua

---
+++ Archive/April 2019/Fling.lua
@@ -0,0 +1,21 @@
+--[[ // METADATA
+
+Last Modified: 4/4/2019 9:14PM
+Author: JackMcJagger15
+
+--]]
+
+power = 500 -- change this to make it more or less powerful
+
+game:GetService('RunService').Stepped:connect(function()
+game.Players.LocalPlayer.Character.Head.CanCollide = false
+game.Players.LocalPlayer.Character.Torso.CanCollide = false
+game.Players.LocalPlayer.Character["Left Leg"].CanCollide = false
+game.Players.LocalPlayer.Character["Right Leg"].CanCollide = false
+end)
+
+wait(.1)
+local bambam = Instance.new("BodyThrust")
+bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+bambam.Force = Vector3.new(power,0,power)
+bambam.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position 
