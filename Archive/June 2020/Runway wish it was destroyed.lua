391086e0c455d64e9d92b5afe759849302849099
timedmarch <59752762+CenteredSniper@users.noreply.github.com>
2024-05-20 05:02:56 -0700

    Create Runway wish it was destroyed.lua

---
+++ Archive/June 2020/Runway wish it was destroyed.lua
@@ -0,0 +1,358 @@
+--[[ // METADATA
+
+Last Modified: 6/29/2020 6:01PM
+Author: Me
+
+--]]
+
+--[[
+woah bro
+funy script made by kawakie#3999
+give credit
+kthx edit the below if you KNOW what you're doing
+]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local UserInputService = game:GetService("UserInputService")
+
+
+
+ScreenGui0 = Instance.new("ScreenGui")
+ImageLabel1 = Instance.new("ImageLabel")
+ImageLabel2 = Instance.new("ImageLabel")
+TextLabel3 = Instance.new("TextLabel")
+TextButton4 = Instance.new("TextButton")
+ImageLabel5 = Instance.new("ImageLabel")
+TextLabel6 = Instance.new("TextBox")
+ImageLabel7 = Instance.new("ImageLabel")
+TextButton8 = Instance.new("TextButton")
+ImageLabel9 = Instance.new("ImageLabel")
+TextLabel10 = Instance.new("TextLabel")
+TextLabel11 = Instance.new("TextLabel")
+TextButton12 = Instance.new("TextButton")
+ImageLabel13 = Instance.new("ImageLabel")
+ScreenGui0.Parent = game.Players.LocalPlayer.PlayerGui
+ScreenGui0.ResetOnSpawn = false
+ScreenGui0.DisplayOrder = 999999999
+ScreenGui0.Name = "kawakie#3999"
+ImageLabel1.Name = "Frame"
+ImageLabel1.Parent = ScreenGui0
+ImageLabel1.Position = UDim2.new(0.100000001, 0, 0.5, 0)
+ImageLabel1.Size = UDim2.new(0.200000003, 0, 0.200000003, 0)
+ImageLabel1.AnchorPoint = Vector2.new(0, 0.5)
+ImageLabel1.BackgroundColor = BrickColor.new("Institutional white")
+ImageLabel1.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel1.BackgroundTransparency = 1
+ImageLabel1.ZIndex = 999999995
+ImageLabel1.Image = "rbxassetid://3570695787"
+ImageLabel1.ImageColor3 = Color3.new(0.160784, 0.168627, 0.184314)
+ImageLabel1.ScaleType = Enum.ScaleType.Slice
+ImageLabel1.SliceCenter = Rect.new(100, 100, 100, 100)
+ImageLabel1.SliceScale = 0.04
+ImageLabel2.Name = "TOPBAR"
+ImageLabel2.Parent = ImageLabel1
+ImageLabel2.Size = UDim2.new(1, 0, 0.100000001, 0)
+ImageLabel2.BackgroundColor = BrickColor.new("Institutional white")
+ImageLabel2.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel2.BackgroundTransparency = 1
+ImageLabel2.ZIndex = 999999996
+ImageLabel2.Image = "rbxassetid://3570695787"
+ImageLabel2.ImageColor3 = Color3.new(0.211765, 0.223529, 0.247059)
+ImageLabel2.ScaleType = Enum.ScaleType.Slice
+ImageLabel2.SliceCenter = Rect.new(100, 100, 100, 100)
+ImageLabel2.SliceScale = 0.04
+TextLabel3.Parent = ImageLabel2
+TextLabel3.Size = UDim2.new(0.899999976, 0, 1, 0)
+TextLabel3.BackgroundColor = BrickColor.new("Institutional white")
+TextLabel3.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel3.BackgroundTransparency = 1
+TextLabel3.ZIndex = 999999997
+TextLabel3.Font = Enum.Font.Gotham
+TextLabel3.FontSize = Enum.FontSize.Size14
+TextLabel3.Text = "Runway | Wish it was destroyed!"
+TextLabel3.TextColor = BrickColor.new("Institutional white")
+TextLabel3.TextColor3 = Color3.new(1, 1, 1)
+TextLabel3.TextScaled = true
+TextLabel3.TextSize = 14
+TextLabel3.TextWrap = true
+TextLabel3.TextWrapped = true
+TextButton4.Name = "EXIT"
+TextButton4.Parent = ImageLabel2
+TextButton4.Position = UDim2.new(1, 0, 0, 0)
+TextButton4.Size = UDim2.new(0.100000001, 0, 1, 0)
+TextButton4.AnchorPoint = Vector2.new(1, 0)
+TextButton4.BackgroundColor = BrickColor.new("Really red")
+TextButton4.BackgroundColor3 = Color3.new(1, 0, 0)
+TextButton4.BackgroundTransparency = 1
+TextButton4.BorderSizePixel = 0
+TextButton4.ZIndex = 999999997
+TextButton4.Font = Enum.Font.Gotham
+TextButton4.FontSize = Enum.FontSize.Size14
+TextButton4.Text = "X"
+TextButton4.TextColor = BrickColor.new("Really black")
+TextButton4.TextColor3 = Color3.new(0, 0, 0)
+TextButton4.TextScaled = true
+TextButton4.TextSize = 14
+TextButton4.TextWrap = true
+TextButton4.TextWrapped = true
+ImageLabel5.Name = "TextButton_Roundify_4px"
+ImageLabel5.Parent = TextButton4
+ImageLabel5.Position = UDim2.new(0.5, 0, 0.5, 0)
+ImageLabel5.Size = UDim2.new(1, 0, 1, 0)
+ImageLabel5.Active = true
+ImageLabel5.AnchorPoint = Vector2.new(0.5, 0.5)
+ImageLabel5.BackgroundColor = BrickColor.new("Institutional white")
+ImageLabel5.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel5.BackgroundTransparency = 1
+ImageLabel5.Selectable = true
+ImageLabel5.ZIndex = 999999996
+ImageLabel5.Image = "rbxassetid://3570695787"
+ImageLabel5.ImageColor3 = Color3.new(1, 0, 0)
+ImageLabel5.ScaleType = Enum.ScaleType.Slice
+ImageLabel5.SliceCenter = Rect.new(100, 100, 100, 100)
+ImageLabel5.SliceScale = 0.04
+TextLabel6.Name = "ID"
+TextLabel6.Parent = ImageLabel1
+TextLabel6.Position = UDim2.new(0.0199999996, 0, 0.949999988, 0)
+TextLabel6.Size = UDim2.new(0.699999988, 0, 0.200000003, 0)
+TextLabel6.AnchorPoint = Vector2.new(0, 1)
+TextLabel6.BackgroundColor = BrickColor.new("Institutional white")
+TextLabel6.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel6.BackgroundTransparency = 1
+TextLabel6.BorderSizePixel = 0
+TextLabel6.ZIndex = 999999997
+TextLabel6.Font = Enum.Font.Gotham
+TextLabel6.FontSize = Enum.FontSize.Size14
+TextLabel6.Text = "ID here!"
+TextLabel6.TextColor = BrickColor.new("Really black")
+TextLabel6.TextColor3 = Color3.new(0, 0, 0)
+TextLabel6.TextSize = 14
+ImageLabel7.Name = "TextLabel_Roundify_4px"
+ImageLabel7.Parent = TextLabel6
+ImageLabel7.Position = UDim2.new(0.5, 0, 0.5, 0)
+ImageLabel7.Size = UDim2.new(1, 0, 1, 0)
+ImageLabel7.AnchorPoint = Vector2.new(0.5, 0.5)
+ImageLabel7.BackgroundColor = BrickColor.new("Institutional white")
+ImageLabel7.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel7.BackgroundTransparency = 1
+ImageLabel7.ZIndex = 999999996
+ImageLabel7.Image = "rbxassetid://3570695787"
+ImageLabel7.ScaleType = Enum.ScaleType.Slice
+ImageLabel7.SliceCenter = Rect.new(100, 100, 100, 100)
+ImageLabel7.SliceScale = 0.04
+TextButton8.Name = "PLAY"
+TextButton8.Parent = ImageLabel1
+TextButton8.Position = UDim2.new(0.949999988, 0, 0.949999988, 0)
+TextButton8.Size = UDim2.new(0.200000003, 0, 0.200000003, 0)
+TextButton8.AnchorPoint = Vector2.new(1, 1)
+TextButton8.BackgroundColor = BrickColor.new("Institutional white")
+TextButton8.BackgroundColor3 = Color3.new(1, 1, 1)
+TextButton8.BackgroundTransparency = 1
+TextButton8.BorderSizePixel = 0
+TextButton8.ZIndex = 999999997
+TextButton8.Font = Enum.Font.Gotham
+TextButton8.FontSize = Enum.FontSize.Size14
+TextButton8.Text = "Play"
+TextButton8.TextColor = BrickColor.new("Really black")
+TextButton8.TextColor3 = Color3.new(0, 0, 0)
+TextButton8.TextSize = 14
+TextButton8.TextWrap = true
+TextButton8.TextWrapped = true
+ImageLabel9.Name = "TextButton_Roundify_4px"
+ImageLabel9.Parent = TextButton8
+ImageLabel9.Position = UDim2.new(0.5, 0, 0.5, 0)
+ImageLabel9.Size = UDim2.new(1, 0, 1, 0)
+ImageLabel9.Active = true
+ImageLabel9.AnchorPoint = Vector2.new(0.5, 0.5)
+ImageLabel9.BackgroundColor = BrickColor.new("Institutional white")
+ImageLabel9.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel9.BackgroundTransparency = 1
+ImageLabel9.Selectable = true
+ImageLabel9.ZIndex = 999999996
+ImageLabel9.Image = "rbxassetid://3570695787"
+ImageLabel9.ScaleType = Enum.ScaleType.Slice
+ImageLabel9.SliceCenter = Rect.new(100, 100, 100, 100)
+ImageLabel9.SliceScale = 0.04
+TextLabel10.Name = "ForceMusic"
+TextLabel10.Parent = ImageLabel1
+TextLabel10.Position = UDim2.new(0, 0, 0.649999976, 0)
+TextLabel10.Size = UDim2.new(1, 0, 0.100000001, 0)
+TextLabel10.BackgroundColor = BrickColor.new("Institutional white")
+TextLabel10.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel10.BackgroundTransparency = 1
+TextLabel10.ZIndex = 999999997
+TextLabel10.Font = Enum.Font.Gotham
+TextLabel10.FontSize = Enum.FontSize.Size14
+TextLabel10.Text = "- = Force Music = -"
+TextLabel10.TextColor = BrickColor.new("Institutional white")
+TextLabel10.TextColor3 = Color3.new(1, 1, 1)
+TextLabel10.TextScaled = true
+TextLabel10.TextSize = 14
+TextLabel10.TextWrap = true
+TextLabel10.TextWrapped = true
+TextLabel11.Name = "HatSpam"
+TextLabel11.Parent = ImageLabel1
+TextLabel11.Position = UDim2.new(0, 0, 0.100000001, 0)
+TextLabel11.Size = UDim2.new(1, 0, 0.100000001, 0)
+TextLabel11.BackgroundColor = BrickColor.new("Institutional white")
+TextLabel11.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel11.BackgroundTransparency = 1
+TextLabel11.ZIndex = 999999997
+TextLabel11.Font = Enum.Font.Gotham
+TextLabel11.FontSize = Enum.FontSize.Size14
+TextLabel11.Text = "- = Hat Spam/Lag Server = -"
+TextLabel11.TextColor = BrickColor.new("Institutional white")
+TextLabel11.TextColor3 = Color3.new(1, 1, 1)
+TextLabel11.TextScaled = true
+TextLabel11.TextSize = 14
+TextLabel11.TextWrap = true
+TextLabel11.TextWrapped = true
+TextButton12.Parent = ImageLabel1
+TextButton12.Position = UDim2.new(0.0199999996, 0, 0.25, 0)
+TextButton12.Size = UDim2.new(0.949999988, 0, 0.400000006, 0)
+TextButton12.BackgroundColor = BrickColor.new("Institutional white")
+TextButton12.BackgroundColor3 = Color3.new(1, 1, 1)
+TextButton12.BackgroundTransparency = 1
+TextButton12.BorderSizePixel = 0
+TextButton12.ZIndex = 999999997
+TextButton12.Font = Enum.Font.Gotham
+TextButton12.FontSize = Enum.FontSize.Size14
+TextButton12.Text = "Toggle (false)"
+TextButton12.TextColor = BrickColor.new("Really black")
+TextButton12.TextColor3 = Color3.new(0, 0, 0)
+TextButton12.TextSize = 14
+ImageLabel13.Name = "TextButton_Roundify_4px"
+ImageLabel13.Parent = TextButton12
+ImageLabel13.Position = UDim2.new(0.5, 0, 0.5, 0)
+ImageLabel13.Size = UDim2.new(1, 0, 1, 0)
+ImageLabel13.Active = true
+ImageLabel13.AnchorPoint = Vector2.new(0.5, 0.5)
+ImageLabel13.BackgroundColor = BrickColor.new("Institutional white")
+ImageLabel13.BackgroundColor3 = Color3.new(1, 1, 1)
+ImageLabel13.BackgroundTransparency = 1
+ImageLabel13.Selectable = true
+ImageLabel13.ZIndex = 999999996
+ImageLabel13.Image = "rbxassetid://3570695787"
+ImageLabel13.ScaleType = Enum.ScaleType.Slice
+ImageLabel13.SliceCenter = Rect.new(100, 100, 100, 100)
+ImageLabel13.SliceScale = 0.04
+
+
+function dragGUI(gui)
+spawn(function()
+local dragging
+local dragInput
+local dragStart
+local startPos
+local function update(input)
+    local delta = input.Position - dragStart
+	ImageLabel1:TweenPosition(UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y), "InOut", "Quart", 0.04, true, nil) 
+end
+gui.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+        dragging = true
+        dragStart = input.Position
+        startPos = ImageLabel1.Position
+        
+        input.Changed:Connect(function()
+            if input.UserInputState == Enum.UserInputState.End then
+                dragging = false
+            end
+        end)
+    end
+end)
+gui.InputChanged:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+        dragInput = input
+    end
+end)
+UserInputService.InputChanged:Connect(function(input)
+    if input == dragInput and dragging then
+        update(input)
+    end
+end)
+end)
+end
+
+local HATSPAM = false
+local player = game.Players.LocalPlayer
+
+TextButton8.MouseButton1Click:Connect(function()
+	local PassID = game:GetService("ReplicatedStorage").PassID
+	PassID:FireServer(
+		TextLabel6.Text
+	)
+end)
+
+TextButton4.MouseButton1Click:Connect(function()
+	ScreenGui0:Destroy()
+	HATSPAM = false
+	warn[[
+	okay
+	because executed scripts in executors don't possess physical localscripts
+	i cannot destroy the script
+	so you actually have to disable everything as a alternative
+	kind of annoying but it helps bypass anti-cheats from just detecting new localscripts
+	]]
+end)
+
+TextButton12.MouseButton1Click:Connect(function()
+	HATSPAM = not HATSPAM
+	TextButton12.Text = "Toggle (" ..tostring(HATSPAM).. ")"
+	
+	local ITEM_PURCHASE = game:GetService("ReplicatedStorage").ITEM_PURCHASE
+	local str = string.rep("读写汉字学足艾儿艾", 123)
+	str = string.rep(str, 10)
+	local char = player.Character
+	local connection = char.ChildAdded:Connect(function(c)
+    	if c:IsA("Accessory") and HATSPAM then
+        	wait()
+       		local mesh = c:FindFirstChildOfClass("Mesh") or c:FindFirstChildOfClass("SpecialMesh")
+
+       		if mesh then mesh:Destroy() end
+        	c.Parent = workspace
+    	end
+	end)
+	
+	while wait() and HATSPAM do
+		spawn(function()
+        	ITEM_PURCHASE:InvokeServer(
+            	"Banana Suit",
+            	"46",
+            	"4962552589"
+        	)
+		end)
+	end
+	if HATSPAM == false then connection:Disconnect() end
+end)
+
+
+dragGUI(ImageLabel2)
